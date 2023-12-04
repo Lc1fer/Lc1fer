57bf6ea65f73b73c6f04b2f0ef2855516031653c
Lc1fer <62872954+Lc1fer@users.noreply.github.com>
2023-12-04 19:36:45 +0800

    update

--- Script/fingerprint.js
+++ Script/fingerprint.js
@@ -1,6 +1,6 @@
 var fingerprint = {
     Nexitally: "9a 91 7b 28 ae dd 1b 11 c2 e2 d2 14 fd e5 35 e5 ca d7 4b e1 fb 09 dc 02 0b 92 e2 67 c5 0e 8f 73",
-    YToo: "bb:c8:20:c1:84:8b:ab:9d:41:45:7c:1c:df:0c:0f:7a:1e:ed:6e:21:f7:ed:5e:67:35:9b:15:8c:c7:fe:4c:b4"
+    YToo: ""
 };
 function operator(e, a) {
     var c = $arguments.airport,
